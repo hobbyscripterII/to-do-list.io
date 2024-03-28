087eebd9006e54f6f941a4120aadf66e4b838c36
이주영 <wruomma2@naver.com>
2024-03-28 12:00:15 +0900

    style: UI 수정

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 let arr = [];
 const todolist = document.getElementById('div-todolist');
 
-
-document.querySelector('#saveIcon').addEventListener('click', function() {
-    const form = null;
-
+document.querySelector('#btn-save').addEventListener('click', function() {
+    // 노드 제어를 통한 동적 요소 생성
     const newLi = document.createElement('li');
     const newText = document.createTextNode(text.value);
-
+    console.log(newText);
     const newBtn = document.createElement('button');
     newBtn.classList.add('btn', 'btn-danger');
-    newBtn.id = 'delBtn'; // 해당 요소에 id 추가
+    newBtn.id = 'btn-del'; // 해당 요소에 id 추가
     newBtn.type = 'button'; // 해당 요소에 type 추가
     newBtn.innerText = '삭제'; // text 추가
 
-    // <button class="btn btn-danger" id="delBtn" type="button"></button>
-    console.log(newBtn);
-
-    newLi.append(newText, newBtn);
+    // appendChild는 하나의 자식만 가질 수 있지만 append는 여러 개의 자식을 가질 수 있다.
+    newLi.append(newText, newBtn); // li 태그와 button 태그를 같이 추가
     todolist.appendChild(newLi);
+
     arr.push(text.value);
 });
 
-// document.querySelector('#delBtn').addEventListener('click', function() {
-//     if(confirm('해당 할 일을 삭제하시겠습니까?')) {
-//         alert('삭제가 완료되었습니다.');
-//     } else {
-//         alert('삭제가 취소되었습니다.');
-//     }
-// });
+document.addEventListener('click', function(e) {
+    if(e.target && e.target.id == 'btn-del') {
+        console.log(e);
+        if(confirm('할 일을 삭제하시겠습니까?')) {
+            alert('할 일이 삭제되었습니다.');
+        } else {
+            alert('삭제가 취소되었습니다.');
+        }
+    }
+});
