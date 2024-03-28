2691d5e71d5d9964c739aec648f5051607790e04
이주영 <wruomma2@naver.com>
2024-03-28 10:16:11 +0900

    동적 버튼 추가

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 let arr = [];
-const todolist = document.getElementById('todolist');
+const todolist = document.getElementById('div-todolist');
 
-function save(text) {
+
+document.querySelector('#saveIcon').addEventListener('click', function() {
     const form = null;
 
-    // 동적 폼 append에 추가(아래 코드 수정 필요)
-    todolist.append(text.value);
+    const newLi = document.createElement('li');
+    const newText = document.createTextNode(text.value);
 
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('btn', 'btn-danger');
+    newBtn.id = 'delBtn'; // 해당 요소에 id 추가
+    newBtn.type = 'button'; // 해당 요소에 type 추가
+    newBtn.innerText = '삭제'; // text 추가
+
+    // <button class="btn btn-danger" id="delBtn" type="button"></button>
+    console.log(newBtn);
+
+    newLi.append(newText, newBtn);
+    todolist.appendChild(newLi);
     arr.push(text.value);
-    console.log(arr);
-    alert('할 일이 저장되었습니다.');
-}
+});
+
+// document.querySelector('#delBtn').addEventListener('click', function() {
+//     if(confirm('해당 할 일을 삭제하시겠습니까?')) {
+//         alert('삭제가 완료되었습니다.');
+//     } else {
+//         alert('삭제가 취소되었습니다.');
+//     }
+// });
